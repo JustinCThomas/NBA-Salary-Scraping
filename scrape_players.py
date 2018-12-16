236c86a9163b50a959ef82ee3fa03b2cde516513
Justin Thomas <thomascjustin@gmail.com>
2018-12-16 17:50:48 -0500

    Modularize the url used.

--- scrape_players.py
+++ scrape_players.py
@@ -2,16 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-url = "https://www.basketball-reference.com/contracts/players.html"
+base_url = "https://www.basketball-reference.com"
 
-req = requests.get(url)
+req = requests.get(base_url + "/contracts/players.html")
 content = req.content
 soup = BeautifulSoup(content, "html.parser")
 player_table = soup.find(id="player-contracts")
 player_rows = player_table.find("tbody").find_all("tr", {"data-row": ""})
 
 player_link = player_rows[0].find("a")
-print(player_rows[0])
+
 print(player_link)
 
 # while True:
