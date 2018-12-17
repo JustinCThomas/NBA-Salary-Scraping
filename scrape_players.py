73ffaa69141f12c847820dc974a76eea2ceb3166
Justin Thomas <thomascjustin@gmail.com>
2018-12-16 22:05:20 -0500

    Access data inside comment block

--- scrape_players.py
+++ scrape_players.py
@@ -1,5 +1,6 @@
 import requests
-from bs4 import BeautifulSoup
+# import re
+from bs4 import BeautifulSoup, Comment
 from time import sleep
 
 base_url = "https://www.basketball-reference.com"
@@ -11,9 +12,45 @@ player_table = soup.find(id="player-contracts")
 player_rows = player_table.find("tbody").find_all("tr", {"data-row": ""})
 
 
-for i in player_rows:
-    if i.find("a") == None:
-        continue
+# for i in player_rows:
+#     if i.find("a") == None:
+#         continue
+#
+#     player_link = i.find("a")
+#     print(player_link)
 
-    player_link = i.find("a")
-    print(player_link)
+def search_players(amount=100):
+    for i in player_rows:
+        if amount <= 0:
+            break
+        if i.find("a") == None:
+            continue
+
+        player_link = i.find("a")['href']
+
+        req = requests.get(base_url + player_link)
+        content = req.content
+        soup = BeautifulSoup(content, "html.parser")
+
+        comment_container = soup.find(id="all_all_salaries")
+        comments = comment_container.find(text=lambda text:isinstance(text, Comment))
+        comment_soup = BeautifulSoup(comments, "html.parser")
+
+        salary_table = comment_soup.find(id="all_salaries")
+
+    
+        salaries = salary_table.find("tbody").find_all("tr", {"data-row": ""})
+        print(salaries)
+
+        for i in salaries:
+            season = i.find("th").get_text()
+            salary = i.find(_class="right")["csk"]
+
+            print("Season: %s Salary: %s" % (season, salary))
+        print(soup)
+        # remove later
+        break
+        amount -= 1
+
+if __name__ == "__main__":
+    search_players()
